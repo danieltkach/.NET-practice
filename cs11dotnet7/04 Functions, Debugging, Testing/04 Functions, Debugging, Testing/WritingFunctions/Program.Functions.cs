7b36b3b74843eac9194e053dda4f57fb79196daf
Daniel Tkach <danieltkach@gmail.com>
2023-06-12 20:46:31 -0300

    basic functions

---
+++ cs11dotnet7/04 Functions, Debugging, Testing/04 Functions, Debugging, Testing/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,25 @@
+﻿partial class Program
+{
+    static void TimesTable(byte number, byte size = 10)
+    {
+        ForegroundColor = ConsoleColor.Green;
+        WriteLine($"Multiplication Table for {number}");
+        ForegroundColor = ConsoleColor.Red;
+        for (int i = 1; i <= size; i++)
+        {
+            WriteLine($"{i} x {number} = {i*number}");
+        }
+        ResetColor();
+    }
+
+    static decimal CalculateTax(decimal amount, string location)
+    {
+        decimal result = location switch
+        {
+            "USA" => amount * 8.25M,
+            "ARG" => amount * 25.70M,
+            _ => amount
+        };
+        return result;
+    }
+}
---
+++ cs11dotnet7/04 Functions, Debugging, Testing/04 Functions, Debugging, Testing/WritingFunctions/Program.cs
@@ -0,0 +1,5 @@
+﻿TimesTable(7);
+
+decimal productCost = 199M;
+WriteLine($"The product will cost ${CalculateTax(productCost, "USA")} in the USA and " +
+    $"${CalculateTax(productCost, "ARG")} in Argentina.");
