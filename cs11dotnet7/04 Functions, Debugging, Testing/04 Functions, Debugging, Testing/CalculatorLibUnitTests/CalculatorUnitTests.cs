af49915d4486c9fe78ff6c11b050188040851481
Daniel Tkach <danieltkach@gmail.com>
2023-06-13 10:57:17 -0300

    unit tests with xUnit

---
+++ cs11dotnet7/04 Functions, Debugging, Testing/04 Functions, Debugging, Testing/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -0,0 +1,39 @@
+using CalculatorLib;
+
+namespace CalculatorLibUnitTests
+{
+    public class CalculatorUnitTests
+    {
+        [Fact]
+        public void TestAdding2And2()
+        {
+            // Arrange
+            double a = 2;
+            double b = 2;
+            double expected = 4;
+            Calculator calc = new();
+
+            // Act
+            double actual = calc.Add(a, b);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAdding2And3()
+        {
+            // Arrange
+            double a = 2;
+            double b = 3;
+            double expected = 5;
+            Calculator calc = new();
+
+            // Act
+            double actual = calc.Add(a, b); 
+            
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
