e6883217075fe295d32296fa5cf6a1a5e47e733a
Daniel Tkach <danieltkach@gmail.com>
2023-06-14 14:02:59 -0300

    enums

--- cs11dotnet7/05 Building Types with OOP/Building Types with OOP/PacktLibrarynetStandard2/Person.cs
+++ cs11dotnet7/05 Building Types with OOP/Building Types with OOP/PacktLibrarynetStandard2/Person.cs
@@ -1,7 +1,11 @@
-﻿namespace Packt.Shared;
+﻿using PacktLibrarynetStandard2;
+
+namespace Packt.Shared;
 
 public class Person
 {
     public string? Name;
     public DateTime DateOfBirth;
+    public WondersOfTheAncientWorld FavoriteAncientWonder;
+    public WondersOfTheAncientWorld BucketList;
 }
--- cs11dotnet7/05 Building Types with OOP/Building Types with OOP/PeopleApp/Program.cs
+++ cs11dotnet7/05 Building Types with OOP/Building Types with OOP/PeopleApp/Program.cs
@@ -1,4 +1,23 @@
 ﻿using Packt.Shared;
+using PacktLibrarynetStandard2;
 
 Person bob = new();
 WriteLine(bob.ToString());
+
+bob.Name = "Bob Smith";
+bob.DateOfBirth = new DateTime(1965, 12, 22);
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+    arg0: bob.Name,
+    arg1: bob.DateOfBirth);
+
+bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+WriteLine(
+    format: "{0} favorite wonder is {1}. its integer is {2}",
+    arg0: bob.Name,
+    arg1: bob.FavoriteAncientWonder,
+    arg2: (int)bob.FavoriteAncientWonder);
+
+bob.BucketList =
+    WondersOfTheAncientWorld.HangingGardensOfBabylon
+    | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}.");
