f1c19c426eb5ef95890d7d447d675b4a1ac9e75e
Daniel Tkach <danieltkach@gmail.com>
2023-06-14 16:26:59 -0300

    Fields (static, constant, read-only, with constructors)

---
+++ cs11dotnet7/05 Building Types with OOP/Building Types with OOP/PacktLibrarynetStandard2/BankAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace PacktLibrarynetStandard2;
+
+public class BankAccount
+{
+    // Instance members
+    public string AccountName;
+    public decimal Balance;
+    
+    // Shared members
+    public static decimal InterestRate;
+}
