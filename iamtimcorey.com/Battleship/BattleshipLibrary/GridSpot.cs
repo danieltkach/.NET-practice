86a96601088e2a6558ed7bfbcb136974fbfc26bf
Daniel Tkach <danieltkach@gmail.com>
2023-06-16 23:44:33 -0300

    battleship library setup

---
+++ iamtimcorey.com/Battleship/BattleshipLibrary/GridSpot.cs
@@ -0,0 +1,27 @@
+﻿namespace BattleshipLibrary
+{
+    public class GridSpot
+    {
+		private string spotLetter;
+		private int spotNumber;
+		private string status;
+
+		public string SpotLetter
+		{
+			get { return spotLetter; }
+			set { spotLetter = value; }
+		}
+
+		public int SpotNumber
+		{ 
+			get { return spotNumber; }
+			set { spotNumber = value; } 
+		}
+
+		public string Status
+		{
+			get { return status; }
+			set { status = value; }
+		}
+	}
+}
---
+++ iamtimcorey.com/Battleship/BattleshipLibrary/PlayerInfo.cs
@@ -0,0 +1,29 @@
+﻿namespace BattleshipLibrary
+{
+    public class PlayerInfo
+    {
+        public string Username { get; set; }
+		private List<GridSpot> shipLocations = new();
+		private List<GridSpot> shotsGrid = new();
+
+		public List<GridSpot> ShipLocations
+		{
+			get { return shipLocations; }
+		}
+
+		public void AddShipLocation(GridSpot spot)
+		{
+			shipLocations.Add(spot);
+		}
+
+		public List<GridSpot> ShotsGrid
+		{
+			get { return shotsGrid; }
+		}
+
+		public void AddShot(GridSpot spot)
+		{
+			shotsGrid.Add(spot);
+		}
+	}
+}
