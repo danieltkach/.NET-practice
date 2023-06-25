425c13ae0ee5cc14ba72a1fd5933b2471f4b9b0b
Daniel Tkach <danieltkach@gmail.com>
2023-06-24 22:14:09 -0300

    card game setup

--- iamtimcorey.com/FirstConsoleProjects/CardGameUI/Program.cs
+++ iamtimcorey.com/FirstConsoleProjects/CardGameUI/Program.cs
@@ -6,10 +6,66 @@ using System.Threading.Tasks;
 
 namespace CardGame
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+
+            Console.ReadLine();
         }
+    }
+
+    public abstract class Deck
+    {
+        private List<PlayingCard> fullDeck = new List<PlayingCard>();
+        private List<PlayingCard> drawPile = new List<PlayingCard>();
+        private List<PlayingCard> discardPile = new List<PlayingCard>();
+
+        public void CreateDeck()
+        {
+
+        }
+
+        public virtual void ShuffleDeck()
+        {
+
+        }
+
+        public abstract List<PlayingCard> DealCard();
+
+        public virtual PlayingCard RequestCard()
+        {
+
+        }
+    }
+
+    public class PlayingCard
+    {
+        public CardSuit Suit { get; set; }
+        public CardValue Value { get; set; }
+    }
+
+    public enum CardSuit
+    {
+        Hearts,
+        Clubs,
+        Diamonds,
+        Spades
+    }
+
+    public enum CardValue
+    {
+        Ace,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Jack,
+        Queen,
+        King
     }
 }
