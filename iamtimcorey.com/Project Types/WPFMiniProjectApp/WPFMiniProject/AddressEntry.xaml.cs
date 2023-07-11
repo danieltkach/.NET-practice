e1d470a4c078c35d802c2da3ad370596edd3a54f
Daniel Tkach <danieltkach@gmail.com>
2023-07-10 23:53:40 -0300

    WPF mini project

---
+++ iamtimcorey.com/Project Types/WPFMiniProjectApp/WPFMiniProject/AddressEntry.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFMiniProject
+{
+    /// <summary>
+    /// Interaction logic for AddressEntry.xaml
+    /// </summary>
+    public partial class AddressEntry : Window
+    {
+        ISaveAddress _parent;
+        public AddressEntry(ISaveAddress parent)
+        {
+            InitializeComponent();
+            _parent = parent;
+        }
+
+        private void SaveAddress_Click(object sender, RoutedEventArgs e)
+        {
+            AddressModel address = new AddressModel
+            {
+                StreetAddress = streetAddressText.Text,
+                City = cityText.Text,
+                State = stateText.Text,
+                ZipCode = zipCodeText.Text,
+            };
+
+            if (!string.IsNullOrEmpty(address.StreetAddress))
+            {
+                _parent.SaveAddress(address);
+            }
+
+            Close();
+        }
+    }
+}
