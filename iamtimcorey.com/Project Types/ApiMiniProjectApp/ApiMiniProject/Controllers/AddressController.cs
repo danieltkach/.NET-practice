1be062fed04181acf9d180b1bea1b7c7922bd7f9
Daniel Tkach <danieltkach@gmail.com>
2023-07-16 11:31:04 -0300

    ASP api mini project

---
+++ iamtimcorey.com/Project Types/ApiMiniProjectApp/ApiMiniProject/Controllers/AddressController.cs
@@ -0,0 +1,26 @@
+﻿using ApiMiniProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace ApiMiniProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AddressController : ControllerBase
+    {
+        private readonly ILogger<AddressController> _logger;
+
+        public AddressController(ILogger<AddressController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST: api/Address
+        [HttpPost]
+        public void Post([FromBody] AddressModel data)
+        {
+            string jsonData = JsonSerializer.Serialize(data);
+            _logger.LogInformation("The address was logged as {Address}", jsonData);
+        }
+    }
+}
---
+++ iamtimcorey.com/Project Types/ApiMiniProjectApp/ApiMiniProject/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using ApiMiniProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace ApiMiniProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        // POST: api/Person
+        [HttpPost]
+        public void Post([FromBody] PersonModel data)
+        {
+            string jsonData = JsonSerializer.Serialize(data);
+            _logger.LogInformation("The person was logged as {Person}", jsonData);
+        }
+
+    }
+}
