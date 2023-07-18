c9f2511579b9359c6a1a0c1580f41ae86d7c8a09
Daniel Tkach <danieltkach@gmail.com>
2023-07-18 11:49:15 -0300

    Blazor Server practice

---
+++ iamtimcorey.com/Project Types/BlazorServerMiniProjectApp/BlazorServerMiniProject/Models/AddressModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerMiniProject.Models;
+
+public class AddressModel
+{
+    [Required]
+    public string? StreetAddress { get; set; }
+
+    [Required]
+    public string? City { get; set; }
+
+    [Required]
+    public string? State { get; set; }
+
+    [Required]
+    [StringLength(10, MinimumLength = 4)]
+    public string? ZipCode { get; set; }
+}
